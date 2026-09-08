e5e90ba810d64b19e4c4eae5fdb7d7490800009e
Andy <A01753419@tec.mx>
2026-09-08 17:11:42 -0600

    Agente y simulacion del agente

--- models/fuego.py
+++ models/fuego.py
@@ -1,5 +1,8 @@
 import numpy as np
 import random
+import random
+from models.mapa import PARED, PUERTA_CERRADA, PUERTA_ABIERTA, DANO_MAXIMO
+
 
 ROWS_FUEGO = 6
 COLS_FUEGO = 8
@@ -15,17 +18,63 @@ def crear_matriz_fuego():
         matrix[fila - 1, columna - 1] = 2
     return matrix
 
-def explosion(r, c, matrix):
+def _valor_borde(paredes_verticales, paredes_horizontales, r1, c1, r2, c2):
+    if r1 == r2 and abs(c1 - c2) == 1:
+        return paredes_verticales[r1, max(c1, c2)]
+    if c1 == c2 and abs(r1 - r2) == 1:
+        return paredes_horizontales[max(r1, r2), c1]
+    return PARED
+
+
+def _danar_pared(danos_verticales, danos_horizontales, r1, c1, r2, c2):
+    if r1 == r2 and abs(c1 - c2) == 1:
+        limite = max(c1, c2)
+        if danos_verticales[r1, limite] < DANO_MAXIMO:
+            danos_verticales[r1, limite] += 1
+            return 1
+    elif c1 == c2 and abs(r1 - r2) == 1:
+        limite = max(r1, r2)
+        if danos_horizontales[limite, c1] < DANO_MAXIMO:
+            danos_horizontales[limite, c1] += 1
+            return 1
+    return 0
+
+
+def _quitar_puerta(paredes_verticales, paredes_horizontales, r1, c1, r2, c2):
+    if r1 == r2 and abs(c1 - c2) == 1:
+        paredes_verticales[r1, max(c1, c2)] = PARED
+    elif c1 == c2 and abs(r1 - r2) == 1:
+        paredes_horizontales[max(r1, r2), c1] = PARED
+
+def explosion(r, c, matrix, paredes_verticales, paredes_horizontales, danos_verticales, danos_horizontales):
     direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     for dr, dc in direcciones:
-        nr, nc = r + dr, c + dc
-        while 0 <= nr < ROWS_FUEGO and 0 <= nc < COLS_FUEGO and matrix[nr, nc] == 2:
-            nr += dr
-            nc += dc
-        if 0 <= nr < ROWS_FUEGO and 0 <= nc < COLS_FUEGO:
-            matrix[nr, nc] = 2
+        fr, fc = r, c
+        while True:
+            nr, nc = r + dr, c + dc
+            if not (0 <= nr < ROWS_FUEGO and 0 <= nc < COLS_FUEGO):
+                break  # fuera del tablero, no hay nada que hacer en esta dirección
+            
+            borde = _valor_borde(paredes_verticales, paredes_horizontales, fr, fc, nr, nc)
+            if borde == PARED:
+                dano_total += _danar_pared(danos_verticales, danos_horizontales, fr, fc, nr, nc)
+                break
+            
+            if borde == PUERTA_CERRADA:
+                _quitar_puerta(paredes_verticales, paredes_horizontales, fr, fc, nr, nc)
+                break
+            if matrix[nr, nc] == 2:
+                fr, fc = nr, nc  # sigue la onda de choque por esta celda en fuego
+                continue
+            
+            if matrix[nr, nc] == 1:
+                matrix[nr, nc] = 2
+            else:
+                matrix[nr, nc] = 2
+            break
+    return dano_total
 
-def flashover(matrix):
+def flashover(matrix, paredes_verticales, paredes_horizontales):
     direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     cambio = True
     while cambio:
@@ -36,22 +85,29 @@ def flashover(matrix):
                     continue
                 for dr, dc in direcciones:
                     ni, nj = i + dr, j + dc
-                    if 0 <= ni < ROWS_FUEGO and 0 <= nj < COLS_FUEGO and matrix[ni, nj] == 2:
+                    if not (0 <= ni < ROWS_FUEGO and 0 <= nj < COLS_FUEGO):
+                        continue
+                    borde = _valor_borde(paredes_verticales, paredes_horizontales, i, j, ni, nj)
+                    if borde in (PARED, PUERTA_CERRADA):
+                        continue  # el humo no se propaga a través de paredes o puertas cerradas
+                    if matrix[ni, nj] == 2:
                         matrix[i, j] = 2
                         cambio = True
                         break
 
-def advance_fire(matrix):
-    fila = random.randint(1, ROWS_FUEGO)
-    columna = random.randint(1, COLS_FUEGO)
+def advance_fire(matrix, paredes_verticales, paredes_horizontales, danos_verticales, danos_horizontales, rng):
+    fila = rng.randint(1, ROWS_FUEGO)
+    columna = rng.randint(1, COLS_FUEGO)
     r, c = fila - 1, columna - 1
+    dano_nuevo = 0
 
     if matrix[r, c] == 0:
         matrix[r, c] = 1
     elif matrix[r, c] == 1:
         matrix[r, c] = 2
     elif matrix[r, c] == 2:
-        explosion(r, c, matrix)
+        dano_nuevo = explosion(r, c, matrix, paredes_verticales, paredes_horizontales,
+                                danos_verticales, danos_horizontales)
 
-    flashover(matrix)
-    return fila, columna
+    flashover(matrix, paredes_verticales, paredes_horizontales)
+    return fila, columna, dano_nuevo
